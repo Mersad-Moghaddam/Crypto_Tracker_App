24fd02887542f9efff3c956aa15ba6d3b26cc7d6
Mersad <nickmersad81@gmail.com>
2024-10-08 12:16:55 +0330

    Update apiKeys.dart

--- lib/controller/services/api/apiKeys.dart
+++ lib/controller/services/api/apiKeys.dart
@@ -1,4 +1,4 @@
-const String apiKeys = 'CG-g5hioK4ytQuaUNo8RNRhRqTE';
+const String apiKeys = 'your api key';
 
 String cryptoApi =
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=$currency&order=market_cap_desc&per_page=$cryptoCount&page=1&sparkline=false&lovable=$language';
